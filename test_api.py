0465d533038036a08779df705780e6806a5b5d05
Saran Pannasuriyaporn <wallik@Sarans-MacBook-Air.local>
2025-03-11 11:01:13 +0700

    Fix Test API

--- test_api.py
+++ test_api.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-url = "http://127.0.0.1:5000/predict"  # Adjust if running on a different host/port
+url = "http://127.0.0.1:5000/inference/predict"  # Adjust if running on a different host/port
 
 # Define input payload
 data = [
